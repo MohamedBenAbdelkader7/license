3ac02febeb09817a5f8ab751a3f1960f908be61c
Ben Abdelkader <ben764343@gmail.com>
2022-06-10 22:55:27 +0200

    make it better

--- db.js
+++ db.js
@@ -5,6 +5,6 @@ const pool = new Pool({
     database: 'license',
     password: 'root',
     port: 5432,
-    host: 'localhost',
+    host: 'postgres://tlpshpdcehwqfu:52b9a9417503229ff610814f76af51e53928204c99d0ce5b6e1b32bad8e3f808@ec2-52-48-159-67.eu-west-1.compute.amazonaws.com:5432/detoree9pme2vf',
 })
 module.exports = { pool };
